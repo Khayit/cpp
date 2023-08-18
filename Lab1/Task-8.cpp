40a66bebd0e3412f1d8b07e9b5a4b968331c9693
Хайит <khamroevkhayit@gmail.com>
2023-08-18 19:26:58 +0500

    project.cpp

---
+++ Lab1/Task-8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+
+    do
+    {
+        cout << "введите возраст" << endl;
+        cin >> age;
+    } while (age <= 18);
+    {
+        cout << "Добро пожаловать, вы совершеннолетний!" << endl;
+    }
+    return 0;
+}
