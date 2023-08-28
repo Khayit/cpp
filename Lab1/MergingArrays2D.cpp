05e3bbee2a42d01cc41d024b42a31a9ce20efbb2
Хайит <khamroevkhayit@gmail.com>
2023-08-28 17:38:22 +0500

    Исправленный рабочий

--- Lab1/MergingArrays2D.cpp
+++ Lab1/MergingArrays2D.cpp
@@ -4,38 +4,34 @@ using namespace std;
 
 int main()
 {
-    int array2D1[2][2] = {{1, 2},{1, 2}};
-    int array2D2[2][2] = {{2, 3},{2, 3}};
-    int twoArrays2DSum[4][4];
-    int index = 0;
-    
-    for(int i = 0; i < 2; i++)
+    int array2D1[2][2] = {{1, 2}, {1, 2}};
+    int array2D2[2][2] = {{3, 4}, {3, 4}};
+    int twoArrays2DSum[4][2];
+
+    for (int i = 0; i < 2; i++)
     {
-        for(int j = 0; j < 2; j++)
+        for (int j = 0; j < 2; j++)
         {
-            twoArrays2DSum[index][index] = array2D1[i][j];
-            index++;
+            twoArrays2DSum[i][j] = array2D1[i][j];
         }
-        index-=2;
     }
-    
-    for(int i = 0; i < 2; i++)
+
+    for (int i = 0; i < 2; i++)
     {
-        for(int j = 0; j < 2; j++)
+        for (int j = 0; j < 2; j++)
         {
-            twoArrays2DSum[index][index] = array2D2[i][j];
-            index++;
+            twoArrays2DSum[i + 2][j] = array2D2[i][j];
         }
-        index-=2;
     }
-    
+
     cout << "Сумма 2х массивов 2Д: " << endl;
-    for(int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
-        for(int j = 0; j < 4; j++)
+        for (int j = 0; j < 2; j++)
         {
-            cout << twoArrays2DSum[i][j] << " " << endl;
+            cout << twoArrays2DSum[i][j] << " ";
         }
+        cout << endl;
     }
     return 0;
 }
