796d72783723b47e944860956e759f91d2a61954
Хайит <khamroevkhayit@gmail.com>
2023-09-17 14:54:22 +0500

    Project

--- Lab1/Project/tempCodeRunnerFile.cpp
+++ Lab1/Project/tempCodeRunnerFile.cpp
@@ -4,32 +4,14 @@
 
 using namespace std;
 
-void addEmployee(string* names, int* ages, float* salaries, int index) {
-    cout << "Введите имя сотрудника: ";
-    cin >> names[index];
-    cout << "Введите возраст сотрудника: ";
-    cin >> ages[index];
-    cout << "Введите зарплату сотрудника: ";
-    cin >> salaries[index];
-}
+void addEmployee(string *names, int *ages, float *salaries, int index);
+void printEmployee(const string &name, int age, float salary);
+void printAllEmployees(const string *names, const int *ages, const float *salaries, int count);
 
-void printEmployee(const string& name, int age, float salary) {
-    cout << "Имя: " << name << endl;
-    cout << "Возраст: " << age << endl;
-    cout << "Зарплата: " << salary << "$" << endl;
-}
+int main()
+{
 
-void printAllEmployees(const string* names, const int* ages, const float* salaries, int count) {
-    for (int i = 0; i < count; i++) {
-        cout << "Сотрудник #" << i+1 << ":" << endl;
-        printEmployee(names[i], ages[i], salaries[i]);
-        cout << endl;
-    }
-}
-
-int main() {
-
-setlocale(LC_ALL, "RU");
+    setlocale(LC_ALL, "RU");
 
     const int maxEmployees = 100;
     string names[maxEmployees];
@@ -40,7 +22,8 @@ setlocale(LC_ALL, "RU");
     cout << "Введите количество сотрудников: ";
     cin >> count;
 
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < count; i++)
+    {
         addEmployee(names, ages, salaries, i);
     }
 
@@ -49,4 +32,31 @@ setlocale(LC_ALL, "RU");
 
     system("pause");
     return 0;
+}
+
+void addEmployee(string *names, int *ages, float *salaries, int index)
+{
+    cout << "Введите имя сотрудника: ";
+    cin >> names[index];
+    cout << "Введите возраст сотрудника: ";
+    cin >> ages[index];
+    cout << "Введите зарплату сотрудника: ";
+    cin >> salaries[index];
+}
+
+void printEmployee(const string &name, int age, float salary)
+{
+    cout << "Имя: " << name << endl;
+    cout << "Возраст: " << age << endl;
+    cout << "Зарплата: " << salary << "$" << endl;
+}
+
+void printAllEmployees(const string *names, const int *ages, const float *salaries, int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        cout << "Сотрудник #" << i + 1 << ":" << endl;
+        printEmployee(names[i], ages[i], salaries[i]);
+        cout << endl;
+    }
 }
